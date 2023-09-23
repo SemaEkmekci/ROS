7586935b6f793006f707f19afcb93b7220b7dfbc
Sema Nur ekmekci <ekmekci.semanur@gmail.com>
2023-09-23 23:00:25 +0300

    Add files via upload

---
+++ catkin_ws_cpp/devel/share/gennodejs/ros/ilk_paket/msg/_index.js
@@ -0,0 +1,10 @@
+
+"use strict";
+
+let drone = require('./drone.js');
+let zaman = require('./zaman.js');
+
+module.exports = {
+  drone: drone,
+  zaman: zaman,
+};
---
+++ catkin_ws_cpp/devel/share/ilk_paket/cmake/ilk_paket-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(ilk_paket_MESSAGE_FILES "/home/sema/catkin_ws/src/ilk_paket/msg/drone.msg;/home/sema/catkin_ws/src/ilk_paket/msg/zaman.msg")
+set(ilk_paket_SERVICE_FILES "")
